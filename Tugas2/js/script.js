4d7839ce721a5f47124793267b93330e153bcfff
rifkisyhd <rifkisyahada81@gmail.com>
2024-03-12 01:22:49 +0700

    mencoba solved bug sidebar

--- Tugas2/js/script.js
+++ Tugas2/js/script.js
@@ -11,4 +11,4 @@ function closeNav() {
 window.addEventListener("scroll", function () {
   var navbar = document.querySelector(".navbar");
   navbar.classList.toggle("scrolled", window.scrollY > 0);
-});
+})
